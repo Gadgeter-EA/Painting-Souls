427e0e8270680c929624d71a15f8640309e419d7
Gadgeter-EA <0233905@up.mx>
2022-04-26 00:12:09 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+    float horizontalMove = 0f; // Storing for passing to FixedUpdate
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) jump = true;
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }else if (Input.GetButtonUp("Crouch")) crouch = false;
+        
+    }
+
+    private void FixedUpdate() // Function dedicated to physics
+    {
+        // Move character
+        // Multplying by TimeDeltaTime makes sure the function executes ALWAYS the same, independent of the PC
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Painting-Souls
+ Proyecto Final Fundamentos
